e2f3f5c770e430e976d4db28e9b6e2205e792b52
annamola <annamola12345@gmail.com>
2022-04-28 23:11:17 -0400

    add images

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -4,20 +4,11 @@ import SocialMedia from "../../components/socialMedia/SocialMedia";
 import { greeting } from "../../portfolio";
 import { Fade } from "react-reveal";
 import FeelingProud from "./FeelingProud";
-// import { projects } from "../../portfolio.js";
-// import ProjectCard from "../../components/ProjectCard/ProjectCard";
+import { projects } from "../../portfolio.js";
+import ProjectCard from "../../components/ProjectCard/ProjectCard";
 
 export default function Greeting(props) {
     const theme = props.theme;
-    // const history = useHistory();
-
-    // const styles = style({
-    //   backgroundColor: `${theme.accentBright}`,
-    //   ":hover": {
-    //     boxShadow: `0 5px 15px ${theme.accentBright}`,
-    //   },
-    // });
-
     return (
         <Fade bottom duration={2000} distance="40px">
             <div className="greet-main" id="greeting">
@@ -71,11 +62,11 @@ export default function Greeting(props) {
                     </div>
                     <div className="greeting-image-div">
                         <FeelingProud theme={theme} />
-                        {/* <div className="repo-cards-div-main">
+                        <div className="repo-cards-div-main">
                             {projects.data.map((repo) => {
                                 return <ProjectCard repo={repo} theme={theme} />;
                             })}
-                        </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
